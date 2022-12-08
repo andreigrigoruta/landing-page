bdb4019b9383deeb962772c1e1a23206187ae2cb
Andrei Grigoruta <androk20@gmail.com>
2022-12-08 19:29:19 +0200

    Tidy up js

--- script.js
+++ script.js
@@ -6,26 +6,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function check(event) {
     if (
-      navMenu.classList.contains("active") &&
-      !event.target.isEqualNode(navMenu) &&
-      !navMenu.contains(event.target) &&
-      !hamburger.contains(event.target)
+      !navMenu.classList.contains("active") ||
+      event.target.isEqualNode(navMenu) ||
+      navMenu.contains(event.target) ||
+      hamburger.contains(event.target)
     ) {
-      hamburger.classList.remove("active");
-      navMenu.classList.remove("active");
-      document.removeEventListener("click", check);
+      return;
     }
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    document.removeEventListener("click", check);
   }
 
   function mobileMenu() {
-    if (!hamburger.classList.contains("active")) {
-      hamburger.classList.add("active");
-      navMenu.classList.add("active");
+    const action = hamburger.classList.contains("active") ? "remove" : "add";
+
+    if (action === "add") {
+      hamburger.classList[action]("active");
+      navMenu.classList[action]("active");
       document.addEventListener("click", check);
     } else {
-      hamburger.classList.remove("active");
-      navMenu.classList.remove("active");
-      document.removeEventListener("click", check);
+      hamburger.classList[action]("active");
+      navMenu.classList[action]("active");
+      document[`${action}EventListener`]("click", check);
     }
   }
 });
